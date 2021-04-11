80a4c6dc721d6eef8c3e662d52bae463f8c1c4b5
alvynah <alvynahw@gmail.com>
2021-04-11 10:58:16 +0300

    Add a method to save a new user and test to ensure it works

--- user.py
+++ user.py
@@ -2,7 +2,12 @@ class User:
     """
     Class that generates new instances of users
     """
-    User_List =[]; # Empty User list
+    user_list =[]; # Empty User list
+    def save_user(self):
+        '''
+        save_user method saves contact objects into user_list
+        '''
+        User.user_list.append(self)
     def __init__(self, full_name, password):
         self.full_name=full_name
         self.password=password
--- user_test.py
+++ user_test.py
@@ -19,6 +19,15 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.full_name,"Alvynah Wabwoba")
         self.assertEqual(self.new_user.password,"Secho")
 
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the contact list
+        '''
+        self.new_user.save_user()  # saving the new contact
+        self.assertEqual(len(User.user_list), 1)
+
+
 if __name__ == '__main__':
     unittest.main()
     
